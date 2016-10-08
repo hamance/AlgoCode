b193e191ae440ec7170c459a58063f048271db98
hamance <htpln@foxmail.com>
2016-10-08 21:38:03 +0800

    INIT: empty projects

---
+++ DataStructure/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by hamance on 2016/10/8.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void main(){
+    cout<<"Hello DataStructure!"<<endl;
+}
---
+++ leetCode/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by hamance on 2016/10/8.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+void main() {
+    cout<<"Hello leetCode!"<<endl;
+}
